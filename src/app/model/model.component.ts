63661dae19a8303a15d3b8eb9a4b51cf5626706d
Yatimisi <yatimisi2012@gmail.com>
2019-06-06 11:13:25 +0800

    Model Driven formBuilder

--- src/app/model/model.component.ts
+++ src/app/model/model.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, FormArray, Validators } from '@angular/forms';
-import { debounceTime } from 'rxjs/Operators';
 
 @Component({
   selector: 'app-model',
@@ -9,20 +8,53 @@ import { debounceTime } from 'rxjs/Operators';
 })
 export class ModelComponent implements OnInit {
 
+  states = ['A', 'B', 'C'];
+
   userForm = new FormGroup({
     name: new FormControl()
   });
 
-  constructor() { }
+  teacherForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit() {
-    this.userForm.controls.name.valueChanges
-    .pipe(
-      debounceTime(500)
-    )
-    .subscribe(
-      data => console.log(data)
-    );
+    this.teacherForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      secretLairs: this.formBuilder.array([]),
+    });
+    this.setAddresses([new Address('1'), new Address('2')]);
   }
 
+  setAddresses(addresses: Address[]) {
+    const addressFGs = addresses.map(address => this.formBuilder.group(address));
+    const addressFormArray = this.formBuilder.array(addressFGs);
+    this.teacherForm.setControl('secretLairs', addressFormArray);
+  }
+
+  get secretLairs(): FormArray {
+    return this.teacherForm.get('secretLairs') as FormArray;
+  }
+
+}
+
+export class Address {
+  street = '';
+  city = '';
+  state = '';
+  zip = '';
+  constructor(street?: string, city?: string, state?: string, zip?: string) {
+    this.street = street;
+    this.city = city;
+    this.state = state;
+    this.zip = zip;
+  }
+}
+
+export class Teacher {
+  id = 0;
+  name = '';
+  addresses: Address[];
 }
